bbb7d57037fcc02ce9c44b217ab092538482b473
noxcodes <samingosi97@gmail.com>
2024-10-28 09:11:04 +0300

    find address and print enbvironment variables

--- environ.c
+++ environ.c
@@ -12,13 +12,13 @@ extern char **environ;
 
 int main(int argc, char *argv[], char *envp[])
 {
-  /* int first, second; */
+  int first, second;
 
   (void) argc;
   (void) argv;
   printf("Let's print using the thirs arg to main\n");
   printf("The address of envp is %p \n", (void *)envp);
-  /*for (first = 0; envp[first]; first++)
+  for (first = 0; envp[first]; first++)
     {
       printf("%d:  %s\n",first,envp[first]);
     }
@@ -26,7 +26,7 @@ int main(int argc, char *argv[], char *envp[])
   for (second = 0; environ[second]; second++)
     {
       printf("%d:  %s\n", second, environ[second]);
-      }*/
+    }
   printf("The address of environ is %p \n", (void *) environ);
   return (0);
 }
